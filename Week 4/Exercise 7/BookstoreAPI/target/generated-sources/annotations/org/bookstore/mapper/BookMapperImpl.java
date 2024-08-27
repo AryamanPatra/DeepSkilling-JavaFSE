3a8a146ba396f354e9e1c8f41b3cee3d2308ef6b
Aryaman Patra <aryamanprivate26@gmail.com>
2024-08-27 17:47:14 +0530

    Completed assignment 4 actually

--- Week 4/Exercise 7/BookstoreAPI/target/generated-sources/annotations/org/bookstore/mapper/BookMapperImpl.java
+++ Week 4/Exercise 7/BookstoreAPI/target/generated-sources/annotations/org/bookstore/mapper/BookMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-21T00:16:10+0530",
-    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
+    date = "2024-08-27T17:46:04+0530",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
 public class BookMapperImpl implements BookMapper {
